7c400c480df0a9e11cb2fdf1e73e5145ebe8c8d6
Leslie Lopez <leslie2515lopez@gmail.com>
2025-01-23 23:59:24 -0600

    Set up modal for profile

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,10 +1,74 @@
 import React from "react";
 import "./Profile.css";
 
-const Profile = ({ isProfileOpen, toggleModal }) => {
+const Profile = ({
+  isProfileOpen,
+  toggleModal,
+  onEmailChange,
+  onPasswordChange,
+}) => {
   return (
     <div className="profile-modal">
-      <button onClick={toggleModal}>Click</button>
+      <article className="br3 ba b--black-40 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center bg-navy">
+        <main className="pa4 w-80 white">
+          <img
+            src="http://tachyons.io/img/logo.jpg"
+            className="h3 w3 dib"
+            alt="avatar"
+          />
+          <h1>John Doe</h1>
+          <h4>Image Submitted: 5</h4>
+          <p>Member since: January</p>
+          <hr />
+          <label className="mt2 fw6" htmlFor="user-name">
+            Name:
+          </label>
+          <input
+            className="pa2 ba w-100"
+            placeholder="john"
+            type="text"
+            name="user-name"
+            id="name"
+          />
+          <label className="mt2 fw6" htmlFor="user-age">
+            Age:
+          </label>
+          <input
+            className="pa2 ba w-100"
+            placeholder="56"
+            type="text"
+            name="user-age"
+            id="age"
+          />
+          <label className="mt2 fw6" htmlFor="user-pet">
+            Pet:
+          </label>
+          <input
+            className="pa2 ba w-100"
+            placeholder="john"
+            type="text"
+            name="user-pet"
+            id="pet"
+          />
+          <div
+            className="mt4"
+            style={{ display: "flex", justifyContent: "space-evenly" }}
+          >
+            <button className="b pa2 grow pointer hover-white w-40 bg-green b--black-20">
+              Save
+            </button>
+            <button
+              className="b pa2 grow pointer hover-white w-40 bg-red b--black-20"
+              onClick={toggleModal}
+            >
+              Cancel
+            </button>
+          </div>
+        </main>
+        <div className="modal-close" onClick={toggleModal}>
+          &times;
+        </div>
+      </article>
     </div>
   );
 };
