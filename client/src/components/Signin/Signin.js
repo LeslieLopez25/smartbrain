f7a52438bc67784db92d1920b11b4d7e813d4530
Leslie Lopez <leslie2515lopez@gmail.com>
2025-01-28 01:12:28 -0600

    Removed comments

--- client/src/components/Signin/Signin.js
+++ client/src/components/Signin/Signin.js
@@ -19,7 +19,6 @@ export default function Signin({ loadUser, onRouteChange }) {
     setSignInData({ ...signInData, signInPassword: event.target.value });
   };
 
-  // Changing backend link
   const onSubmitSignIn = (data) => {
     setSignInData({ ...signInData, loading: true });
     fetch("https://smartbrain-api-2mk1.onrender.com/signin", {
