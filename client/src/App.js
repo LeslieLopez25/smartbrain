53da082f03f1067bd9002eab76f9085f175ae903
Leslie Lopez <leslie2515lopez@gmail.com>
2025-02-04 01:14:59 -0600

    Updated app.js for frontend

--- client/src/App.js
+++ client/src/App.js
@@ -78,8 +78,18 @@ export default function App() {
     <AuthProvider>
       <div className="App">
         <ParticlesBg
+          class="particles-bg-canvas-self"
           type="thick"
-          bg={{ position: "fixed", height: "100%", width: "100%" }}
+          bg={{
+            position: "fixed",
+            height: "100%",
+            width: "100%",
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
+            zIndex: -1,
+          }}
         />
         <Suspense
           fallback={
