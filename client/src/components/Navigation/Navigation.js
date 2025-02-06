aa1ae798b404584cd8affc3a89c93711861449de
Leslie Lopez <leslie2515lopez@gmail.com>
2025-02-05 23:25:49 -0600

    Updated navigation component with auth0

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,34 +1,32 @@
 import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 import ProfileImage from "../Profile/ProfileImage";
 
-export default function Navigation({ onRouteChange, isSignedIn, toggleModal }) {
-  if (isSignedIn) {
-    return (
-      <nav
-        style={{
-          display: "flex",
-          justifyContent: "flex-end",
-        }}
-      >
-        <ProfileImage onRouteChange={onRouteChange} toggleModal={toggleModal} />
-      </nav>
-    );
-  } else {
-    return (
-      <nav style={{ display: "flex", justifyContent: "flex-end" }}>
-        <p
-          onClick={() => onRouteChange("signin")}
-          className="f3 link dim white underline pa3 pointer"
-        >
-          Sign In
-        </p>
-        <p
-          onClick={() => onRouteChange("register")}
-          className="f3 link dim white underline pa3 pointer"
-        >
-          Register
-        </p>
-      </nav>
-    );
-  }
+export default function Navigation({ toggleModal }) {
+  const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
+
+  return (
+    <nav style={{ display: "flex", justifyContent: "flex-end" }}>
+      {isAuthenticated ? (
+        <>
+          <ProfileImage toggleModal={toggleModal} />
+          <p
+            onClick={() => logout({ returnTo: window.location.origin })}
+            className="f3 link dim white underline pa3 pointer"
+          >
+            Sign Out
+          </p>
+        </>
+      ) : (
+        <>
+          <p
+            onClick={() => loginWithRedirect()}
+            className="f3 link dim white underline pa3 pointer"
+          >
+            Sign In
+          </p>
+        </>
+      )}
+    </nav>
+  );
 }
