5da51f1080ad78b3a4d1880a65f5c37bf5cde8dd
Leslie Lopez <leslie2515lopez@gmail.com>
2025-02-12 23:30:55 -0600

    Updated rank.js

--- client/src/components/Rank/Rank.js
+++ client/src/components/Rank/Rank.js
@@ -1,11 +1,8 @@
 export default function Rank({ name, entries }) {
-  console.log("Rank Component Props:", { name, entries });
-
   return (
     <div>
       <p className="white f3">{`${name}, your current entry count is...`}</p>
-      <p className="white f1">{entries ?? "0"}</p>{" "}
-      {/* Fallback to 0 if undefined */}
+      <p className="white f1">{Number(entries) || 0}</p>
     </div>
   );
 }
