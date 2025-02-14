093c39ffce65b2ef6591ee3f90d4cb831d2858d9
Leslie Lopez <leslie2515lopez@gmail.com>
2025-02-13 21:35:57 -0600

    Updated Signin.js

--- client/src/components/Signin/Signin.js
+++ client/src/components/Signin/Signin.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import LoadingScreen from "react-loading-screen";
+import BASE_URL from "../../config";
+
 import "./Signin.css";
 
 export default function Signin() {
@@ -12,9 +14,7 @@ export default function Signin() {
       bgColor="transparent"
       spinnerColor="#ffffff"
       textColor="#ffffff"
-      logoSrc=""
       text="Loading..."
-      className="text-xl w-fit mx-auto backdrop-blur-sm"
     >
       <article className="br3 ba b--black-40 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
         <main className="pa4 white">
@@ -24,14 +24,19 @@ export default function Signin() {
             {!isAuthenticated ? (
               <button
                 onClick={() => loginWithRedirect()}
-                className="b ph3 pv2 input-reset ba b--black white bg-transparent grow pointer f6 dib"
+                className="b ph3 pv2 input-reset ba b--white white bg-transparent grow pointer f6 dib"
               >
                 Sign In with Auth0
               </button>
             ) : (
               <button
-                onClick={() => logout({ returnTo: window.location.origin })}
-                className="b ph3 pv2 input-reset ba b--black white bg-transparent grow pointer f6 dib"
+                onClick={() =>
+                  logout({
+                    returnTo:
+                      process.env.REACT_APP_BASE_URL || window.location.origin,
+                  })
+                }
+                className="b ph3 pv2 input-reset ba b--white white bg-transparent grow pointer f6 dib"
               >
                 Logout
               </button>
