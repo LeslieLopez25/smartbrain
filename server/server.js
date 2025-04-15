83fe4a0d6aa2dc19d63000b05b3b0eef585fa8cd
Leslie Lopez <leslie2515lopez@gmail.com>
2025-04-14 23:55:02 -0600

    Revert "Added user authentication"
    This reverts commit fc15e441139c98ce0d7a5093b2c28aeabcc2f9b1.

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.get("/", (req, res) => {
   res.send(db.users);
 });
 
-app.post("/signin", signin.signinAuthentication(db, bcrypt));
+app.post("/signin", signin.handleSignin(db, bcrypt));
 
 app.post("/register", (req, res) => {
   register.handleRegister(req, res, bcrypt, db);
