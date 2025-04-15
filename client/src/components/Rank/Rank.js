56b7519d77db3d508fc13fbd79e8626841d42cfe
Leslie Lopez <leslie2515lopez@gmail.com>
2025-04-14 23:53:28 -0600

    Revert "Updated rank.js"
    This reverts commit 5da51f1080ad78b3a4d1880a65f5c37bf5cde8dd.

--- client/src/components/Rank/Rank.js
+++ client/src/components/Rank/Rank.js
@@ -1,8 +1,11 @@
 export default function Rank({ name, entries }) {
+  console.log("Rank Component Props:", { name, entries });
+
   return (
     <div>
       <p className="white f3">{`${name}, your current entry count is...`}</p>
-      <p className="white f1">{Number(entries) || 0}</p>
+      <p className="white f1">{entries ?? "0"}</p>{" "}
+      {/* Fallback to 0 if undefined */}
     </div>
   );
 }
