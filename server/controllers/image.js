2bc4920aa3490485067229adec7a03a2308a82f1
Leslie Lopez <leslie2515lopez@gmail.com>
2025-04-14 23:54:46 -0600

    Revert "Updated backend to use auth0"
    This reverts commit 3cc8a0463d87f970c2da0a98f4ee1feebabb1a56.

--- server/controllers/image.js
+++ server/controllers/image.js
@@ -8,25 +8,22 @@ const app = new Clarifai.App({
 const handleApiCall = (req, res) => {
   app.models
     .predict("face-detection", req.body.input)
-    .then((data) => res.json(data))
-    .catch((err) => res.status(400).json("Unable to work with API"));
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.status(400).json("unable to work with API"));
 };
 
 const handleImage = (req, res, db) => {
-  const auth0_id = req.auth.sub;
-
+  const { id } = req.body;
   db("users")
-    .where({ auth0_id })
+    .where("id", "=", id)
     .increment("entries", 1)
     .returning("entries")
     .then((entries) => {
-      if (entries.length) {
-        res.json(entries[0].entries);
-      } else {
-        res.status(404).json("User not found");
-      }
+      res.json(entries[0].entries);
     })
-    .catch((err) => res.status(500).json("Unable to update entries"));
+    .catch((err) => res.status(400).json("unable to get entries"));
 };
 
 module.exports = {
--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const bodyParser = require("body-parser"); // latest version of exressJS now comes with Body-Parser!
+const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
 const knex = require("knex");
-const { checkJwt } = require("./auth");
+const checkJwt = require("./auth");
+
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
 const profile = require("./controllers/profile");
 const image = require("./controllers/image");
 
@@ -14,26 +19,34 @@ const db = knex({
 });
 
 const app = express();
+const router = express.Router();
+
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Server is running");
+  res.send(db.users);
 });
 
-app.get("/profile", checkJwt, (req, res) => {
+app.post("/signin", signin.signinAuthentication(db, bcrypt));
+
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+
+app.get("/profile/:id", (req, res) => {
   profile.handleProfileGet(req, res, db);
 });
 
-app.put("/profile", checkJwt, (req, res) => {
-  profile.handleProfileUpdate(req, res, db);
+router.get("/profile", checkJwt, (req, res) => {
+  res.json({ message: "This is a protected route", user: req.auth });
 });
 
-app.put("/image", checkJwt, (req, res) => {
+app.put("/image", (req, res) => {
   image.handleImage(req, res, db);
 });
 
-app.post("/imageurl", checkJwt, (req, res) => {
+app.post("/imageurl", (req, res) => {
   image.handleApiCall(req, res);
 });
 
