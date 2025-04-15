d6a048f8dda637afcbb73cbd7f840896b13c5b21
Leslie Lopez <leslie2515lopez@gmail.com>
2025-04-14 23:53:54 -0600

    Revert "Updated facerecongnition.js"
    This reverts commit 9d784d9874a0400c1c0c061f258d44e56bec0a59.

--- client/src/components/FaceRecognition/FaceRecognition.js
+++ client/src/components/FaceRecognition/FaceRecognition.js
@@ -1,58 +1,34 @@
-import React, { useMemo } from "react";
-import PropTypes from "prop-types";
-
+import React from "react";
 import "./FaceRecognition.css";
 
 export default function FaceRecognition({ imageUrl, boxes }) {
-  const renderBoxes = useMemo(() => {
-    if (!boxes || boxes.length === 0) {
-      return <p className="white f3">No faces detected</p>;
-    }
-
-    return boxes.map((box, index) => (
-      <div
-        key={index}
-        className="bounding-box"
-        style={{
-          top: box.topRow,
-          right: box.rightCol,
-          bottom: box.bottomRow,
-          left: box.leftCol,
-        }}
-      ></div>
-    ));
-  }, [boxes]);
-
   return (
     <div className="center ma">
       <div className="absolute mt2">
-        {imageUrl ? (
-          <>
-            <img
-              id="inputimage"
-              alt=""
-              src={imageUrl}
-              width="500px"
-              height="auto"
-            />
-            {renderBoxes}
-          </>
+        <img
+          id="inputimage"
+          alt=""
+          src={imageUrl}
+          width="500px"
+          height="auto"
+        />
+        {boxes.length > 0 ? (
+          boxes.map((box, index) => (
+            <div
+              key={index}
+              className="bounding-box"
+              style={{
+                top: box.topRow,
+                right: box.rightCol,
+                bottom: box.bottomRow,
+                left: box.leftCol,
+              }}
+            ></div>
+          ))
         ) : (
-          <p className="white f3">No Image Uploaded</p>
+          <p className="white f3">No faces detected</p>
         )}
       </div>
     </div>
   );
 }
-
-FaceRecognition.propTypes = {
-  imageUrl: PropTypes.string,
-  boxes: PropTypes.arrayOf(
-    PropTypes.shape({
-      topRow: PropTypes.number.isRequired,
-      rightCol: PropTypes.number.isRequired,
-      bottomRow: PropTypes.number.isRequired,
-      leftCol: PropTypes.number.isRequired,
-    })
-  ),
-};
