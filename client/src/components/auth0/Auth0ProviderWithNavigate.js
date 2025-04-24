63a2dc4fed3c4dd915e2df06ea89ba20618ce5a2
Leslie Lopez <leslie2515lopez@gmail.com>
2025-04-23 23:53:53 -0600

    Added comments to auth0 file

--- client/src/components/auth0/Auth0ProviderWithNavigate.js
+++ client/src/components/auth0/Auth0ProviderWithNavigate.js
@@ -1,12 +1,14 @@
 import { Auth0Provider } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 
+// Wraps the app in Auth0Provider and handles redirect navigation
 const Auth0ProviderWithNavigate = ({ children }) => {
   const navigate = useNavigate();
 
   const domain = process.env.REACT_APP_AUTH0_DOMAIN;
   const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
+  // Called after Auth0 login to redirect the user
   const onRedirectCallback = (appState) => {
     navigate(appState?.returnTo || window.location.pathname);
   };
