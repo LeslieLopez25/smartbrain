e6cc33c3b787f7c1210c6ca1366261f5e335720a
Leslie Lopez <leslie2515lopez@gmail.com>
2025-04-24 00:02:16 -0600

    Added comments to image link form file

--- client/src/components/ImageLinkForm/ImageLinkForm.js
+++ client/src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./ImageLinkForm.css";
 
+// This component renders the input field and button for face detection
 export default function ImageLinkForm({ onInputChange, onButtonSubmit }) {
   return (
     <div>
