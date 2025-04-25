8260ff6ca609fd0ee82522d039e30ede913cd2f2
Leslie Lopez <leslie2515lopez@gmail.com>
2025-04-24 22:40:00 -0600

    Added comments to config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,4 @@
+// If there's an environment variable set for the base URL (e.g., in production),
+// use that. Otherwise, default to your local backend server.
 export const API_URL =
   process.env.REACT_APP_BASE_URL || "http://localhost:3001";
